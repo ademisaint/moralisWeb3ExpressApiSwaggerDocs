3dce86c19e403c8d31dc83ba1b21975e574894f4
ademisaint <saintademilayo@gmail.com>
2023-02-12 19:00:58 +0100

    configured URL

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://raw.githubusercontent.com/ademisaint/moralisWeb3ExpressApiSwaggerDocs/main/moralisWeb3ExpressApi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
